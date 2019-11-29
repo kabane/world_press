eb54a570a0419e97ea9dcfeb3bb1f50edb19827c
kabane <kabaneshi@gmail.com>
2019-11-29 22:49:59 +0900

    Scaffold Project

---
+++ src/main/java/com/example/world_press/WorldPressApplication.java
@@ -0,0 +1,13 @@
+package com.example.world_press;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldPressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldPressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/world_press/WorldPressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.world_press;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldPressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
