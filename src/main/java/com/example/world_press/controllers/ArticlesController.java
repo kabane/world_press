eee956969794fb8db6c85b762f43a1d94de58832
kabane <kabaneshi@gmail.com>
2019-12-02 11:35:14 +0900

    微修正

--- src/main/java/com/example/world_press/controllers/ArticlesController.java
+++ src/main/java/com/example/world_press/controllers/ArticlesController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/articles")
